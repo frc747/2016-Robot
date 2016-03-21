fc2053730f0949270c5607c1d87d0abff3e26ba9
Tyler Roussos <tylerroussos@gmail.com>
2016-03-21 12:08:31 -0400

    Refactored Autonomous Code to use separate configuration files.

---
+++ src/org/usfirst/frc/team869/robot/AutonomousConfig.java
@@ -0,0 +1,47 @@
+package org.usfirst.frc.team869.robot;
+
+/**
+ * Distances are in inches
+ * Rotations are in degrees
+ * Times are in seconds
+ * Speed values are in motor %
+ */
+public final class AutonomousConfig {
+    
+    private AutonomousConfig() {
+    } 
+    
+    //TODO Determine Values for these classes
+    public final class SpyBox{
+        
+        private SpyBox(){
+        }
+        
+        public static final int FIRST_DISTANCE  = 100, 
+                                SECOND_DISTANCE = 100,
+                                ROTATION        = -10;
+        
+        public static final double SHOOTER_STOP = 10,
+                                   INTAKE_START = 1.5,
+                                   INTAKE_STOP  = 10,
+                                   DRIVE_SPEED  = 0.5;
+    }
+    
+    public final class ReachDefenses{
+        
+        private ReachDefenses(){
+        }
+        
+        public static final double SPEED    = -.25,
+                                   DISTANCE = 7.75;  
+    }
+    
+    public final class CrossDefense{
+        
+        private CrossDefense(){
+        }
+        
+        public static final double SPEED    = -.5,
+                                   DISTANCE = 50;
+    }
+}
