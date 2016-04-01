35b3835d753488eb4ef5f326e66a920558e94c0c
ckirschner <corey.kirschner@gmail.com>
2016-03-31 21:26:09 -0400

    Moved the climber over to the new valve design

--- src/org/usfirst/frc/team869/robot/subsystems/ClimberSubsystem.java
+++ src/org/usfirst/frc/team869/robot/subsystems/ClimberSubsystem.java
@@ -8,18 +8,26 @@ import org.usfirst.frc.team869.robot.commands.ClimbRetractCommand;
 
 public class ClimberSubsystem extends Subsystem {
     
-    private DoubleSolenoid climbSolenoid = new DoubleSolenoid (RobotMap.Solenoid.CLIMB_EXTEND, RobotMap.Solenoid.CLIMB_RETRACT);
+//    private DoubleSolenoid climbSolenoid = new DoubleSolenoid (RobotMap.Solenoid.CLIMB_EXTEND, RobotMap.Solenoid.CLIMB_RETRACT);
     private Solenoid climbLock = new Solenoid(RobotMap.Solenoid.CLIMB_LOCK);
+    private Solenoid climbExtend = new Solenoid(RobotMap.Solenoid.CLIMB_EXTEND);
+    private Solenoid climbRetract = new Solenoid(RobotMap.Solenoid.CLIMB_RETRACT);
+    
 
     public void initDefaultCommand() {
         setDefaultCommand(new ClimbRetractCommand());
     }
     
     public void climbExtend (){
-        climbSolenoid.set(DoubleSolenoid.Value.kForward);
+//        climbSolenoid.set(DoubleSolenoid.Value.kForward);
+    	climbExtend.set(true);
+    	climbRetract.set(true);
+    	
     }
     public void climbRetract (){
-        climbSolenoid.set(DoubleSolenoid.Value.kReverse);
+//        climbSolenoid.set(DoubleSolenoid.Value.kReverse);
+    	climbExtend.set(false);
+    	climbRetract.set(false);
     }
     public void climbLockOn (){
         climbLock.set(true);
